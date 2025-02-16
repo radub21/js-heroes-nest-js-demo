74dded9c838ef3a31b7aa190dada4617c090c8b8
Radu Buta <radu.buta@rebeldot.com>
2025-02-16 19:17:57 +0200

    showcase dependency injection pattern by adding a monitoring module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MonitoringModule } from './monitoring/monitoring.module';
 
 @Module({
-  imports: [],
+  imports: [MonitoringModule],
   controllers: [AppController],
   providers: [AppService],
 })
